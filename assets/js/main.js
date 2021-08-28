f36c9e0f75522d36e3416314fdeeb63818e6bb96
Naimur53 <naimurrhman53@gmail.com>
2021-08-28 17:26:58 +0600

    preloader done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@
 const searchInput = document.getElementById('input');
 const result = document.getElementById('result');
 const seeMore = document.getElementById('seeMore');
+const gif = document.getElementById('gif');
+
 const search = () => {
     const searchValue = searchInput.value;
     if (searchValue.length == 0) {
@@ -14,12 +16,16 @@ const search = () => {
 }
 
 const loadData = (inputValue) => {
+    result.textContent = ''
+
+    gif.style.display = 'block';
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
     fetch(url)
         .then(res => res.json())
         .then(data => inputData(data));
 }
 const inputData = data => {
+    gif.style.display = 'none';
     searchInput.value = '';
     if (data.meals == null) {
         alert(`We could not find any food match with this"${searchInput.value}" name `)
@@ -32,7 +38,7 @@ const inputData = data => {
             div.classList.add('col');
             div.innerHTML = `
             <div class="card h-100">
-                        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">  
                         <div class="card-body">
                             <h5 class="card-title">${meal.strMeal}</h5>
                             <p class="card-text"> 
@@ -69,7 +75,7 @@ const details = data => {
         <div class="card border-0">
             <div class="row g-0">
                 <div class="col-md-4">
-                <img src="${detail.strMealThumb}" class="img-fluid" alt="..."> 
+                <img src="${detail.strMealThumb}" class="img-fluid" alt="...">  
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
@@ -82,5 +88,6 @@ const details = data => {
         </div> 
         `;
         seeMore.appendChild(div);
+
     })
 }
